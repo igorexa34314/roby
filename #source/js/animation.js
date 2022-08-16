426806b5ea8a8cdb92a47bdc1d459e7c3c2efc7d
Igor <igorkono343@gmail.com>
2022-08-16 21:08:37 +0300

    Full release
    Full landing

--- #source/js/animation.js
+++ #source/js/animation.js
@@ -1,29 +1,4 @@
 
-// Init slider
-document.addEventListener('DOMContentLoaded', function () {
-	const slider = new ChiefSlider('.slider', {
-		swipe: true,
-		loop: true,
-		autoplay: true,
-		interval: 4000,
-	});
-});
-// menu-burger
-const menuBurger = document.querySelector('.burger-menu');
-if (menuBurger) {
-	const menuBody = document.querySelector('.menu__body')
-	menuBurger.addEventListener("click", function () {
-		document.body.classList.toggle('_lock');
-		menuBurger.classList.toggle('_active');
-		menuBody.classList.toggle('_active');
-	});
-}
-// Draw stats (percent)
-$('.stats__item').each(function (index, element) {
-	const value = $(element).children().next().text();
-	$(element).children().last().css("width", value);
-});
-
 // Function Animation on scroll
 function AnimateItems(animWrapper) {
 	var animItems;
--- #source/js/main.js
+++ #source/js/main.js
@@ -1,2 +1,13 @@
 //@prepros-append burger.js
 //@prepros-append stats.js
+//@prepros-append animation.js
+
+// Init slider
+document.addEventListener('DOMContentLoaded', function () {
+	const slider = new ChiefSlider('.slider', {
+		swipe: true,
+		loop: true,
+		autoplay: true,
+		interval: 4000,
+	});
+});
